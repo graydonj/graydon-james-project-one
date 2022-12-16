022cdd0d396bcc20bece601bc8559269e6620862
graydon james <graydonj2@gmail.com>
2022-12-16 15:24:33 -0500

    Added a small 'bump' functionality when we hit the edge of the carousel

--- photo-carousel.js
+++ photo-carousel.js
@@ -21,11 +21,8 @@ rightArrow.addEventListener('click', function() {
     // increment the image
     currImage++;
 
-    // Slide all the images. We do this by translating X by -100% for each
-    carousel.forEach(function (imageItem, i) {
-        const imageItemStyle = imageItem.style;
-        imageItemStyle.transform = `translateX(${(i - currImage) * 100}%)`;
-    })
+    // slide the images. If we go off the edge we get our currImage reset
+    currImage = slideImages(carousel, currImage);
 });
 
 // Listen for a click on the left arrow
@@ -33,9 +30,54 @@ leftArrow.addEventListener('click', function () {
     // decrement the image
     currImage--;
 
-    // Slide all the images. We do this by translating X by 100% for each
-    carousel.forEach(function (imageItem, i) {
-        const imageItemStyle = imageItem.style;
-        imageItemStyle.transform = `translateX(${(i - currImage) * 100}%)`;
-    })
+    // Slide the images...if we go off the "edge", we get our currImage reset
+    currImage = slideImages(carousel, currImage);
 });
+
+function slideImages(carousel, currImage) {
+
+    // if we go "off the edge" past the last image, reset to last image (bump and stop)
+    if (currImage === carousel.length) {
+        console.log(currImage);
+
+        // bump the image, but because of how image bump works, we have to loop the whole bunch of images and for each image in the carousel...bump
+        carousel.forEach(function (imageItem, i) {
+            const imageItemStyle = imageItem.style;
+            imageItemStyle.transform = `translateX(${(i - currImage) * 100}%)`;
+            console.log(imageItemStyle.transform);
+        });
+
+        // then reset each image
+        currImage = carousel.length - 1;
+        console.log(currImage);
+        setTimeout( () => {
+            carousel.forEach(function (imageItem, i) {
+                const imageItemStyle = imageItem.style;
+                imageItemStyle.transform = `translateX(${(i - currImage) * 100}%)`;
+                console.log(imageItemStyle.transform);
+            });
+        });
+    } else if (currImage === -1) {
+
+        // bump the image
+        carousel.forEach(function (imageItem, i) {
+            const imageItemStyle = imageItem.style;
+            imageItemStyle.transform = `translateX(${(i - currImage) * 50}%)`;
+
+            // then reset
+            currImage = 0;
+            setTimeout(() => {
+                imageItemStyle.transform = `translateX(${(i - currImage) * 100}%)`;
+            }, 100);
+        });
+    } else {
+
+        // slide the image over
+        carousel.forEach(function (imageItem, i) {
+            const imageItemStyle = imageItem.style;
+            imageItemStyle.transform = `translateX(${(i - currImage) * 100}%)`;
+        });
+    }
+
+    return currImage;
+}
