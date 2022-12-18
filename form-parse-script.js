d54911e215c93f8e17c47167e9342e1cbb80f20a
graydon james <graydonj2@gmail.com>
2022-12-17 21:31:09 -0500

    Move form parsing JS to the blog page so we can take comments and append them to the comment section; fix some styling issues with the blog page comment section

--- form-parse-script.js
+++ form-parse-script.js
@@ -1,2 +1,8 @@
 const formElement = document.querySelector('form');
 console.log(formElement);
+
+// listen for a click
+// put the elements of our comment into some unvarying variables
+// grab the comment section of the page
+// find the end of it
+// append the current comment at the end, in the format we expect
